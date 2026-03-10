163e5d8dceb3ffffcd97b7d02c7daa85ecb57e26
muhamedfazeel <reachme@fazeel.in>
2026-03-10 12:54:45 +0530

    chore: update s3_bucket module source path

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,11 @@
 # S3 Buckets Module - Using for_each to create multiple buckets
 module "s3_buckets" {
-    source = "./modules/aws-s3/s3-bucket"
-    for_each = var.s3_buckets
+  source   = "./modules/aws-s3-bucket"
+  for_each = var.s3_buckets
 
-    bucket_name         = each.value.bucket_name
-    bucket_prefix       = each.value.bucket_prefix
-    force_destroy       = each.value.force_destroy
-    object_lock_enabled = each.value.object_lock_enabled
-    tags                = merge(var.common_tags, each.value.tags)
+  bucket_name         = each.value.bucket_name
+  bucket_prefix       = each.value.bucket_prefix
+  force_destroy       = each.value.force_destroy
+  object_lock_enabled = each.value.object_lock_enabled
+  tags                = merge(var.common_tags, each.value.tags)
 }
