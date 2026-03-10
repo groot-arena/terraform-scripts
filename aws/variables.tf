d77ce23503b512bf772340b9e73e4b2e58265da9
muhamedfazeel <reachme@fazeel.in>
2026-03-10 12:54:09 +0530

    lint(variables): lint changes

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-    description = "The AWS region to deploy resources in."
-    type        = string
-    default     = "us-south-1"  
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-south-1"
 }
 
 variable "environment" {
-    description = "Environment name (e.g., develop, staging, release)"
-    type        = string
-    default     = "develop"
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
 }
 
 variable "project_name" {
-    description = "Name of the project"
-    type        = string
-    default     = ""
+  description = "Name of the project"
+  type        = string
+  default     = ""
 }
 
 # Common Tags
@@ -25,16 +25,16 @@ variable "common_tags" {
 
 # S3 Buckets Configuration - Map of bucket configurations
 variable "s3_buckets" {
-    description = "Map of S3 bucket configurations."
-    type = map(object({
-        # Basic bucket configuration
-        bucket_name         = optional(string)
-        bucket_prefix       = optional(string)
-        force_destroy       = optional(bool, false)
-        object_lock_enabled = optional(bool, false)
-        enable_versioning   = optional(bool, true)
+  description = "Map of S3 bucket configurations."
+  type = map(object({
+    # Basic bucket configuration
+    bucket_name         = optional(string)
+    bucket_prefix       = optional(string)
+    force_destroy       = optional(bool, false)
+    object_lock_enabled = optional(bool, false)
+    enable_versioning   = optional(bool, true)
 
-        # Tags specific to this bucket
-        tags = optional(map(string), {})
-    }))
+    # Tags specific to this bucket
+    tags = optional(map(string), {})
+  }))
 }
