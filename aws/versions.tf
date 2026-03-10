ed962e5e1b02b7df0eec61582d5ccc9ac715e787
muhamedfazeel <reachme@fazeel.in>
2026-03-10 12:52:41 +0530

    chore: remove backend config from versions.tf

--- aws/versions.tf
+++ aws/versions.tf
@@ -1,15 +1,9 @@
 terraform {
-    required_version = ">= 1.5.3"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>6.0"
-        }
+  required_version = ">= 1.5.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
     }
-    backend "s3" {
-        bucket  = "s3-groot-tfstate"
-        key     = "global/groot/develop/terraform.tfstate"
-        region  = "ap-south-1"
-        encrypt = true
-    }
+  }
 }
