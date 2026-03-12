6c950e09a01e6e9f3e18e2200f96cb2ae8e79a46
muhamedfazeel <reachme@fazeel.in>
2026-03-12 21:17:58 +0530

    fix: move aws_region variable declaration to backend.tf

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources into"
+  type        = string
+  default     = "ap-south-1"
+}
+
 terraform {
   backend "s3" {
     bucket               = "s3-groot-tfstate"
--- aws/variables.tf
+++ aws/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in."
-  type        = string
-  default     = "us-south-1"
-}
-
 variable "environment" {
   description = "Environment name (e.g., dev, staging, prod)"
   type        = string
