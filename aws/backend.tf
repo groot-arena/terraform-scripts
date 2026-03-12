0b2de79a7519408fbb9ece34518d26a96f45fd97
muhamedfazeel <reachme@fazeel.in>
2026-03-12 21:00:03 +0530

    chore(aws/backend.tf): update formatting

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "s3" {
-    bucket  = "s3-groot-tfstate"
-    key     = "global/groot/terraform.tfstate"
-    region  = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt = true
+  backend "s3" {
+    bucket               = "s3-groot-tfstate"
+    key                  = "global/groot/terraform.tfstate"
+    region               = "ap-south-1"
+    dynamodb_table       = "terraform-lock"
+    encrypt              = true
     workspace_key_prefix = "env"
   }
 }
